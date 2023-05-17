40e9bbf1c6388326eb8844b6b12cf7400ad98f6f
Qiqi <114935495+liu-qiqi@users.noreply.github.com>
2023-05-17 18:47:37 +0100

    Update ge_chart1.js

--- js/ge_chart1.js
+++ js/ge_chart1.js
@@ -18,14 +18,7 @@
           }
         }
       },
-      toolbox: {
-        feature: {
-          dataView: { show: true, readOnly: false },
-          magicType: { show: true, type: ['line', 'bar'] },
-          restore: { show: true },
-          saveAsImage: { show: true }
-        }
-      },
+      
       legend: {
         data: ['Employment', 'Cumulative Growth']
       },
